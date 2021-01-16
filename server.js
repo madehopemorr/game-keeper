4ddaa23c4eee2ad10e1ee7307eb9204719a52598
Uyen Nguyen <udnguyen@ucsd.edu>
2021-01-16 04:12:13 -0800

    add autocomplete dropdown for search bar

--- server.js
+++ server.js
@@ -34,9 +34,6 @@ require("./routes/api-routes.js")(app);
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
